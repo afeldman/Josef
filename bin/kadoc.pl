81ca88062364f0c3cf7cdb3881b866ed693b8efc
anton feldmann <anton.feldmann@gmail.com>
2018-08-01 10:08:44 +0900

    full regexp to find routine

--- bin/kadoc.pl
+++ bin/kadoc.pl
@@ -1,14 +1,15 @@
 #!/usr/bin/perl -w
 
+use Doc::Kadoc;
+
 use strict;
 use warnings;
 use 5.010;
 use Getopt::Std;
 $Getopt::Std::STANDARD_HELP_VERSION = 1;
 
-use constant VERSION => '0.0.1';
 use constant HEADER => "===============================\n            KADOC           \n===============================\n\n";
-    
+
 my %options=();
 getopts("hvo:", \%options);
 
@@ -16,13 +17,12 @@ if ( $options{h} )
 {
     print HEADER;
     print "KADOC is a Karel documentation gernator.";
-    print 
     print "\n";
 }
 
 if ( $options{v} ){
     print HEADER;
-    print "VERSION: ",VERSION,"\n";
+    print "VERSION: ", $Doc::Kadoc::VERSION, "\n";
 }
 
 my $output_file = "";
@@ -32,11 +32,40 @@ if ( $options{o} ) {
 
 my $inputfile = "";
 if( defined $ARGV[0]) {
-    $inputfile = $ARGV[0]
+    $inputfile = $ARGV[0];
 }else{
-    print "where is the input karel script!!!\n"
+    print "where is the input karel script!!!\n";
+    exit( 0 );
 }
-
 
 #
 # start building kadoc
+
+# open the input file
+open FILE, $inputfile or die $!;
+# copy the lines into an array
+my @lines = <FILE>;
+# close the file
+close( FILE );
+
+# go through each line
+for ( my $i = 0; $i < scalar( @lines ); $i++ )
+{
+    my $line = $lines[$i];
+
+    # remove all leading spaces
+    $line =~ s/^[\t\s]+//;
+
+    # remove all single command lines without information
+    $line =~ s/^\-\-[\t\s]+$//;
+
+    # remove mutiple inner spaces
+    $line =~ s/[\t\s]+/ /;
+
+    if ( $line =~ /^routine ([\d\w]+)[\(\w*\)]*:*\w*/i ){
+        print $line;
+        print $1
+    }
+
+
+}
--- lib/Doc/KADOC/josef.pm
+++ lib/Doc/KADOC/josef.pm
@@ -1,13 +1,8 @@
-#!/usr/bin/env perl
-
-package Doc::KADOC
+package Doc::Kadoc::josef;
 
 use warnings;
 use strict;
 
-our $VERSION = '0.0.1';
 
-require Exporter;
-@ISA = qw(Exporter);
 
 1;
